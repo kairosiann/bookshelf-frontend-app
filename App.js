b616517d1f20fc758a79f13eac0eb2235220f0f1
kairosiann <kairosiann@gmail.com>
2025-04-10 18:48:42 +0900

    updates to App.js and addition of review screen

--- App.js
+++ App.js
@@ -8,9 +8,39 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar, SafeAreaView } from 'react-native';
+import * as Font from 'expo-font';
 import AppNavigator from './navigation/AppNavigator';
 
+// Main application component with font loading
 export default function App() {
+	const [fontsLoaded, setFontsLoaded] = useState(false);
+
+	// Loading custom fonts
+	useEffects(() => {
+		async function loadFonts() {
+			await Font.loadAsync({
+				'Merriweather': require('./assets/fonts/Merriweather-Regular.ttf'),
+        		'Merriweather-Bold': require('./assets/fonts/Merriweather-Bold.ttf'),
+        		'Inter': require('./assets/fonts/Inter-Regular.ttf'),
+        		'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+        		'IBMPlexSans-Medium': require('./assets/fonts/IBMPlexSans-Medium.ttf'),
+        		'IBMPlexSans-Regular': require('./assets/fonts/IBMPlexSans-Regular.ttf'),
+			});
+			setFontsLoaded(true);
+		}
+
+		loadFonts();
+	}, []);
+
+	// Loading screen while fonts are loading
+	if (!fontsLoaded) {
+		return (
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<Text>Loading...</Text>
+			</View>
+		);
+	}
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
